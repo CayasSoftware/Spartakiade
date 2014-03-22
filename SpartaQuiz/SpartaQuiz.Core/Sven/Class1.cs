dc93667cd43118a24643d4f8e7e06419bb982c8c
Sven Sönnichsen <sven@sven-s.de>
2014-03-22 13:00:11 +0100

    Sven-s Draft for Game Design

---
+++ SpartaQuiz/SpartaQuiz.Core/Sven/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpartaQuiz.Core.Sven
+{
+    public class QuestionItem
+    {
+        public Guid GameSession { get; set; }
+
+        public string Question { get; set; }
+
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+
+        public int CorrectAnswer { get; set; }
+
+    }
+}
