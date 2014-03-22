7d5e4cb0fcc704299baff6127a6ac7346f257a15
Sebastian Seidel <buero@cayas.de>
2014-03-22 15:17:52 +0100

    Android Start

---
+++ SpartaQuiz/SpartaQuiz.Droid/GameActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SpartaQuiz.Droid
+{
+	[Activity(Label = "GameActivity")]			
+	public class GameActivity : Activity
+	{
+		protected override void OnCreate( Bundle savedInstanceState )
+		{
+			base.OnCreate(savedInstanceState);
+
+			if(String.IsNullOrWhiteSpace(Intent.GetStringExtra("Playname")))
+			{
+				Toast.MakeText(this, "Sorry playname not valid!", ToastLength.Long).Show();
+				Finish();
+				return;
+			}
+
+			SetContentView(Resource.Layout.Game);
+		}
+	}
+}
+
--- SpartaQuiz/SpartaQuiz.Droid/MainActivity.cs
+++ SpartaQuiz/SpartaQuiz.Droid/MainActivity.cs
@@ -11,25 +11,25 @@ namespace SpartaQuiz.Droid
 	[Activity(Label = "SpartaQuiz.Droid", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
-		protected override void OnCreate( Bundle bundle )
+		protected override void OnCreate( Bundle savedInstanceState )
 		{
-			base.OnCreate(bundle);
+			base.OnCreate(savedInstanceState);
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
-			
+			Button button = FindViewById<Button>(Resource.Id.GameStart_StartBtn);
 			button.Click += delegate
 			{
-				button.Text = string.Format("{0} clicks!", count++);
+				var playname = FindViewById<EditText>(Resource.Id.GameStart_PlaynameEdtxt).Text;
+
+				var intent = new Intent(this, typeof(GameActivity));
+				intent.PutExtra("Playname", playname);
+
+				StartActivity(intent);
 			};
 		}
 	}
 }
-
-
