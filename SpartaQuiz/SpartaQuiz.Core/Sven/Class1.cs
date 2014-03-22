300324e48e1e498b0bde26f0add5c1cb8ebbbebf
Sven Sönnichsen <sven@sven-s.de>
2014-03-22 13:04:44 +0100

    Sven-s Draft for Game Design

--- SpartaQuiz/SpartaQuiz.Core/Sven/Class1.cs
+++ SpartaQuiz/SpartaQuiz.Core/Sven/Class1.cs
@@ -5,9 +5,42 @@ using System.Text;
 
 namespace SpartaQuiz.Core.Sven
 {
+
+    interface IGame
+    {
+        Guid StartGame(string username);
+
+        QuestionItem GetNextQuestion(Guid gameSessionGiud);
+
+        CheckQuestionResult CheckQuestion(Guid questionGuid, int answerNumber);
+
+        GameSession GetCurrentGameSession(Guid gameSessionGuid);
+
+        void CancelGame(Guid gameSessionGuid);
+
+    }
+
+    public class CheckQuestionResult
+    {
+        public int CorrectAnswer { get; set; }
+    }
+
+    public class GameSession
+    {
+        public Guid GameSessionGuid { get; set; }
+
+        public string Username { get; set; }
+
+        public int Score { get; set; }
+
+        public bool IsGameOver { get; set; }
+    }
+
     public class QuestionItem
     {
-        public Guid GameSession { get; set; }
+        public Guid GameSessionGuid { get; set; }
+
+        public Guid QuestionGuid { get; set; }
 
         public string Question { get; set; }
 
@@ -15,8 +48,6 @@ namespace SpartaQuiz.Core.Sven
         public string AnswerB { get; set; }
         public string AnswerC { get; set; }
         public string AnswerD { get; set; }
-
-        public int CorrectAnswer { get; set; }
 
     }
 }
